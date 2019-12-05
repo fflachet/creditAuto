7f03b30ab6562f81c6132a71f2ada3c3da73b39b
Av4LoN7 <georges.garnier1@gmail.com>
2019-12-05 10:00:42 +0100

    first commit + setup project

---
+++ src/main/java/creditAuto/Client.java
@@ -0,0 +1,15 @@
+package creditAuto;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Client {
+	private Long id;
+	private String firstname;
+	private String lastname;
+	private String adresse;
+	private LocalDate birthday;
+
+}
