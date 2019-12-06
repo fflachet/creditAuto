7d87cb653d787c65960bec14df7a2c73e834442a
Your Name <you@example.com>
2019-12-06 14:20:52 +0100

    Première version des requetes post/get qui fonctionnent

--- src/main/java/creditAuto/SimulationController.java
+++ src/main/java/creditAuto/SimulationController.java
@@ -4,7 +4,9 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,16 +21,23 @@ public class SimulationController {
 	@Autowired
 	private SimulationService newSimulService;
 	
-	@RequestMapping(value = "/{purchaseAmount, loanAmount, loanDuration, vehicleCategory}", method = RequestMethod.POST)
-	@ResponseStatus(value=HttpStatus.CREATED)
-	public void createSimul(@RequestBody Simulation newSimul) {
-		newSimulService.createSimul(newSimul.getPurchaseAmount(), newSimul.getLoanAmount(), newSimul.getLoanDuration(), newSimul.getVehicleCategory());
-	}
+//	@RequestMapping(value = "/{purchaseAmount, loanAmount, loanDuration, vehicleCategory}", method = RequestMethod.POST)
+//	//@RequestMapping(value = "/simulation", method = RequestMethod.POST)
+//	@ResponseStatus(value=HttpStatus.CREATED)
+//	public void createSimul(@RequestBody Simulation newSimul) {
+//		newSimulService.createSimul(newSimul.getPurchaseAmount(), newSimul.getLoanAmount(), newSimul.getLoanDuration(), newSimul.getVehicleCategory());
+//	}
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	//@ResponseStatus(value=HttpStatus.CREATED)
 	public BigDecimal sendBackRate2Front(@RequestBody Simulation newSimul) {
 		return newSimulService.sendBackRate(newSimul);
+	}
+	
+	@GetMapping
+	//@ResponseStatus(value=HttpStatus.CREATED)
+	public Simulation sendBacwdfgwdsfg2Front() {
+		return new Simulation(BigDecimal.ONE, BigDecimal.ONE, 24, VehicleCategory.A);
 	}
 	
 	
