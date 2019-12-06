6e756e4330ade0aa0b7c4b21362a15160fabf624
Av4LoN7 <georges.garnier1@gmail.com>
2019-12-06 11:15:26 +0100

    add corssOrign config + minor debug

--- src/main/java/creditAuto/SimulationController.java
+++ src/main/java/creditAuto/SimulationController.java
@@ -1,20 +1,19 @@
 package creditAuto;
 
-import java.util.List;
+import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class SimulationController {
 	
 	@Autowired
@@ -24,6 +23,12 @@ public class SimulationController {
 	@ResponseStatus(value=HttpStatus.CREATED)
 	public void createSimul(@RequestBody Simulation newSimul) {
 		newSimulService.createSimul(newSimul.getPurchaseAmount(), newSimul.getLoanAmount(), newSimul.getLoanDuration(), newSimul.getVehicleCategory());
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	//@ResponseStatus(value=HttpStatus.CREATED)
+	public BigDecimal sendBackRate2Front(@RequestBody Simulation newSimul) {
+		return newSimulService.sendBackRate(newSimul);
 	}
 	
 	
--- src/main/java/creditAuto/SimulationService.java
+++ src/main/java/creditAuto/SimulationService.java
@@ -3,17 +3,14 @@ package creditAuto;
 import java.math.BigDecimal;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import creditauto.contract.VehicleCategory;
-import fr.diginamic.erwan.spring_mvc.model.User;
 
 @Service
 public class SimulationService {
 	
-	@Autowired
 	private Simulation newSimul;
+	
 	private List<Simulation> listSimul;
 	
 	private BigDecimal rateCalculation(Simulation simul) {
@@ -24,9 +21,12 @@ public class SimulationService {
 	public void createSimul(BigDecimal purchaseAmount, BigDecimal loanAmount, int loanDuration, VehicleCategory vehicleCategory) { 
 		newSimul= new Simulation(purchaseAmount, loanAmount, loanDuration, vehicleCategory);
 		//listSimul.add(newSimul);
-		BigDecimal rateValue=rateCalculation(newSimul);
-		//return rateValue;
 		
+	}
+	
+	public BigDecimal sendBackRate(Simulation newSimul) {
+		BigDecimal rateValue=rateCalculation(newSimul);
+		return rateValue;
 	}
 /*
 	public Simulation findSimul(Long id) { 
@@ -34,8 +34,8 @@ public class SimulationService {
 		
 	}
 	*/
-	public void deleteSimul(Long id) { 
+	/*public void deleteSimul(Long id) { 
 		if 
-	}
+	}*/
 
 }
